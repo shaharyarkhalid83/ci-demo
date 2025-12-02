9c31d556b8cffa332847b6d838e6e455a8644d2a
shaharyarkhalid83 <shaharyarkhalid@gmail.com>
2025-12-02 01:13:26 -0600

    Bump version to v1.1.0

--- app/version.py
+++ app/version.py
@@ -1,6 +1,6 @@
 # app/version.py
 
 # Change this value to demonstrate CI/CD deployments
-APP_VERSION = "v1.0.0"
+APP_VERSION = "v1.1.0"
 
-APP_MESSAGE = "Hello from the CI/CD demo running on ECS!"
+APP_MESSAGE = "Hello from the CI/CD demo running on ECS! after making a modification"
